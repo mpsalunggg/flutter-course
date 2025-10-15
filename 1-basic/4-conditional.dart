0a66eb40395b257695a4afc1269ceaccf45274fe
mpsalunggg <putrasatria893@gmail.com>
2025-10-15 20:44:18 +0700

    switchcase, ternary operator:

--- 1-basic/4-conditional.dart
+++ 1-basic/4-conditional.dart
@@ -1,3 +1,77 @@
+enum Color { red, green, blue }
+
+void ifelse() {
+  int age = 25;
+
+  if (age > 20) {
+    print('Dewasaaa');
+  } else if (age > 17 && age <= 20) {
+    print('Remajaaa');
+  } else {
+    print('Ank anak');
+  }
+}
+
+void ternaryOperator() {
+  int age = 20;
+  String status = age >= 18 ? 'Adult' : 'Minor';
+  print('Age $age: $status');
+
+  int number = 15;
+  String result = number % 2 == 0 ? 'Even' : 'Odd';
+  print('Number $number is $result');
+
+  int score = 85;
+  String grade = score >= 90
+      ? 'A'
+      : score >= 80
+      ? 'B'
+      : score >= 70
+      ? 'C'
+      : score >= 60
+      ? 'D'
+      : 'F';
+  print('Score $score: Grade $grade');
+}
+
+void switchCaseStatement() {
+  String day = 'Monday';
+
+  switch (day) {
+    case 'Monday':
+      print('Start of the work week');
+      break;
+    case 'Tuesday':
+    case 'Wednesday':
+    case 'Thursday':
+      print('Middle of the work week');
+      break;
+    case 'Friday':
+      print('Last work day');
+      break;
+    case 'Saturday':
+    case 'Sunday':
+      print('Weekend!');
+      break;
+    default:
+      print('Invalid day');
+  }
+
+  Color favoriteColor = Color.blue;
+
+  switch (favoriteColor) {
+    case Color.red:
+      print('   Favorite color is Red');
+      break;
+    case Color.green:
+      print('   Favorite color is Green');
+      break;
+    case Color.blue:
+      print('   Favorite color is Blue');
+      break;
+  }
+}
+
 void nullAwareConditions() {
   String? name;
   String? email = 'user@example.com';
@@ -13,6 +87,7 @@ void nullAwareConditions() {
 
   String? text;
   if (text?.isEmpty ?? true) {
+    text = "Hai gaiss";
     print('Text is null or empty');
   }
 
@@ -23,15 +98,15 @@ void typeCasting() {
   dynamic value = 'Hello Dart';
 
   if (value is String) {
-    print('   Value is String: ${value.toUpperCase()}');
+    print('Value is String: ${value.toUpperCase()}');
   }
 
   if (value is int) {
-    print('   Value is Integer: ${value + 10}');
+    print('Value is Integer: ${value + 10}');
   }
 
   if (value is! bool) {
-    print('   Value is not boolean');
+    print('Value is not boolean');
   }
 
   Object text = 'Hello World';
@@ -44,6 +119,13 @@ void typeCasting() {
 }
 
 void main() {
+  ifelse();
+  print("===================");
+  ternaryOperator();
+  print("===================");
+  switchCaseStatement();
+  print("===================");
   nullAwareConditions();
   print("===================");
+  typeCasting();
 }
