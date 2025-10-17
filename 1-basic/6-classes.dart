a48be9486df5d3a266acf7d746ab775aca57af65
mpsalunggg <putrasatria893@gmail.com>
2025-10-17 20:43:19 +0700

    getter setter dart

--- 1-basic/6-classes.dart
+++ 1-basic/6-classes.dart
@@ -3,7 +3,23 @@ class Person {
   int age = 0;
 
   void introduce() {
-    print("   Halo, nama saya $name, umur $age tahun");
+    print("Halo, nama saya $name, umur $age tahun");
+  }
+
+  String get getName {
+    return name;
+  }
+
+  void set setName(String name) {
+    this.name = name;
+  }
+
+  void setAge(int age) {
+    if (age < 0) {
+      print("Umur tidak boleh negatif");
+    } else {
+      this.age = age;
+    }
   }
 }
 
@@ -14,7 +30,10 @@ class Student {
 
   Student(this.name, this.school, this.grade);
 
-  void printInformation(){
+  // for constructor naming
+  Student.testingUser(this.name) : grade = 4;
+
+  void printInformation() {
     print('My name is $name, im from $school , and my grade is $grade');
   }
 }
@@ -33,7 +52,7 @@ void basicClass() {
   print("");
 }
 
-void constructorClass(){
+void constructorClass() {
   Student student1 = Student('Putra', 'SMA 2 PALU', 25);
 
   print(student1.name);
@@ -41,10 +60,28 @@ void constructorClass(){
   print(student1.grade);
 
   student1.printInformation();
+
+  Student testingUser = Student.testingUser("Mps");
+
+  // school can be null
+  print(
+    'name: ${testingUser.name}, school: ${testingUser.school}, grade: ${testingUser.grade}',
+  );
+}
+
+void getterSetterClass() {
+  Person person3 = Person();
+  person3.setName = "Andi";
+  person3.setAge(30);
+  person3.introduce();
+
+  print("Name: ${person3.getName}");
 }
 
 void main() {
   basicClass();
   print('===================');
   constructorClass();
+  print('===================');
+  getterSetterClass();
 }
