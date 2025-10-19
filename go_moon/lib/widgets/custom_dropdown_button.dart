b78624540fe16609cb180d6a5466801d86059d0b
mpsalunggg <putrasatria893@gmail.com>
2025-10-19 22:19:25 +0700

    chore: adjust sequence child dropdown menu item

--- go_moon/lib/widgets/custom_dropdown_button.dart
+++ go_moon/lib/widgets/custom_dropdown_button.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+// import 'package:flutter/widgets.dart';
 
 class CustomDropdownButtonClass extends StatelessWidget {
   List<String> values;
@@ -19,12 +19,12 @@ class CustomDropdownButtonClass extends StatelessWidget {
       child: DropdownButton(
         value: values.first,
         onChanged: (_) {},
-        items: values.map((e) {
-          return DropdownMenuItem(child: Text(e), value: e);
-        }).toList(),
         underline: Container(),
         dropdownColor: const Color.fromRGBO(53, 53, 53, 1.0),
         style: const TextStyle(color: Colors.white),
+        items: values.map((e) {
+          return DropdownMenuItem(value: e, child: Text(e));
+        }).toList(),
       ),
     );
   }
